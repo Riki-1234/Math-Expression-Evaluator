7b4747a6013184f6c2757993bdd995565e9349c0
riki-1234 <49783868+Riki-1234@users.noreply.github.com>
2023-02-18 19:05:04 +0100

    added example

--- examples/example_cpp17_2.cpp
+++ examples/example_cpp17_2.cpp
@@ -4,7 +4,7 @@
 int main() {
 	auto evaluatedExpression = seval::evalExpr("5(5+5) * cos(10)");
 	if (evaluatedExpression) {
-		std::cout << "The result is: " << seval::evalExpr(expression).value();
+		std::cout << "The result is: " << evaluatedExpression.value();
 	}
 	else {
 		std::cout << "Invalid syntax";
