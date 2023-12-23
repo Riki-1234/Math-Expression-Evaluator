31dbe686f119a61df39714e90fefc798b2023058
BosnianDeveloper <legion23012003@gmail.com>
2023-12-23 23:43:21 +0100

    Removed the need for negative functions

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <vector>
 #include "simple-evaluator.h"
 
 int main() {
-	std::string expression = "asinh(10)";
-	auto evaluatedExpression = seval::evalExpr(expression);
-	if (evaluatedExpression) {
-		std::cout << "The result is: " << evaluatedExpression.value();
-	}
-	else {
-		std::cout << "Invalid syntax";
-	}
+	std::vector<std::pair<std::string, double>> expressions {
+		{"-sin(5)", 0.958924}
+	};
+
+    for (const auto& [expression, result] : expressions) {
+        auto evaluatedExpression = seval::evalExpr(expression);
+        if (evaluatedExpression) {
+            std::cout << expression << " | Result: " << evaluatedExpression.value() << " | Expected: " << result << std::endl;
+        }
+        else {
+            std::cout << "Invalid syntax: " << expression << std::endl;
+        }
+    }
 }
